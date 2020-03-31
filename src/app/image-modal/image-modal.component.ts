407e50c4fb27db44743c5a1e76811ff85d787cec
Anders Klint <anders.klint519@gmail.com>
2020-03-31 23:51:53 +0200

    Fix state resume of heart color

--- src/app/image-modal/image-modal.component.ts
+++ src/app/image-modal/image-modal.component.ts
@@ -12,13 +12,14 @@ import { IMAGE_MODAL_DIALOG_DATA } from '../image-modal-overlay.tokens';
 export class ImageModalComponent implements OnInit {
 
   rating: string = '0';
-  heartColor: string = 'rgba(255,100,100,1)'
+  heartColor: string = 'rgba(255,255,255,1)'
   imageMaxHeight: string = '75vh';
   imageMaxWidth: string = '75vw';
   buttonBarTopMargin: string = '10px';
   buttonBarOpacity: string = '1.0';
   isFullscreen: boolean = false;
   maxLikes: number = 5;
+  currentLikes: number = 0;
 
   constructor(
     private imageService: ImageService,
@@ -28,6 +29,9 @@ export class ImageModalComponent implements OnInit {
 
   ngOnInit() {
     this.getRating();
+    this.currentLikes = +localStorage.getItem('like:' + this.image);
+    var colorModifier = this.getHeartColor();
+    this.heartColor = `rgba(255, ${colorModifier}, ${colorModifier}, 1)`
   }
 
   getRating() {
@@ -38,25 +42,27 @@ export class ImageModalComponent implements OnInit {
   }
 
   onLike() {
-    let currentLikes = +localStorage.getItem('like:' + this.image);
-    if (currentLikes < this.maxLikes) {
-      currentLikes += 1;
+    if (this.currentLikes < this.maxLikes) {
+      this.currentLikes += 1;
       this.imageService.likeImage(this.image).subscribe(res => {
             this.rating = res;
           }
         );
     }
     else {
-      currentLikes = 0;
+      this.currentLikes = 0;
       this.imageService.likeImage(this.image, -5).subscribe(res => {
         this.rating = res;
         }
       );
     }
-    var colorModifier = (this.maxLikes - currentLikes) * 255/this.maxLikes;
-    console.log(colorModifier);
+    var colorModifier = this.getHeartColor();
     this.heartColor = `rgba(255, ${colorModifier}, ${colorModifier}, 1)`
-    localStorage.setItem('like:' + this.image, currentLikes.toString());
+    localStorage.setItem('like:' + this.image, this.currentLikes.toString());
+  }
+
+  private getHeartColor() {
+    return (this.maxLikes - this.currentLikes) * 255 / this.maxLikes;
   }
 
   onClose() {
